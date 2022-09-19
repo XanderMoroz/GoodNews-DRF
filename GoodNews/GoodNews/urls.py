2724f661476fc4ad0afc98c9d8b972f8194117c3
Александр <7176uiop3436>
2022-09-19 14:41:17 +0300

    Add paths: pages/, news/

--- GoodNews/GoodNews/urls.py
+++ GoodNews/GoodNews/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pages/', include('django.contrib.flatpages.urls')),
+    # Делаем так, чтобы все адреса из нашего приложения (news/urls.py)
+    # подключались к главному приложению с префиксом products/.
+    path('news/', include('news.urls')),
 ]
