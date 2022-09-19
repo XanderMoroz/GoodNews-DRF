8fc56acdfd9cfb244f543be81d5a2f4337b7bbdd
Александр <7176uiop3436>
2022-09-19 14:42:45 +0300

    Add app_paths: pages/, news/

---
+++ GoodNews/news/urls.py
@@ -0,0 +1,30 @@
+"""McDonalds URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .views import PostList, PostDetail
+
+urlpatterns = [
+    # path — означает путь.
+    # В данном случае путь ко всем товарам у нас останется пустым.
+    # Т.к. наше объявленное представление является классом,
+    # а Django ожидает функцию, нам надо представить этот класс в виде view.
+    # Для этого вызываем метод as_view.
+    path('', PostList.as_view(), name='post_list'),
+    # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
+    # int — указывает на то, что принимаются только целочисленные значения
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+]
